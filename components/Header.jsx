34f7af1fb0fbc8feb789b1c6d6844f19e425ca56
a-n-a-s <mirzaanas815@gmail.com>
2022-01-26 11:52:41 +0530

    2.32hr

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 import styled from "styled-components";
+import Modal from "react-modal";
+import { useRouter } from "next/router";
+import TransferModal from "./modal/TransferModal";
+import Link from "next/link";
+
+Modal.setAppElement("#__next");
 
 const Wrapper = styled.div`
   width: calc(100% - 3rem);
@@ -51,6 +57,24 @@ const WalletAddress = styled.div`
   font-size: 0.8rem;
 `;
 const Header = ({ walletAddress, conncetWallet }) => {
+  const router = useRouter();
+
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      transform: "translate(-50%,-50%)",
+      backgroundColor: "#0a0b0d",
+      padding: "0",
+      border: "none",
+    },
+    overlay: {
+      backgroundColor: "rgba(10,11,13,0.75)",
+    },
+  };
+
   return (
     <Wrapper>
       <Title>Assets</Title>
@@ -64,8 +88,17 @@ const Header = ({ walletAddress, conncetWallet }) => {
         <Button style={{ backgroundColor: "#3773f5", color: "#000" }}>
           Buy / Sell
         </Button>
-        <Button>Send / Receive</Button>
+        <Link href={"/?transfer=1"}>
+          <Button>Send / Receive</Button>
+        </Link>
       </ButtonContainer>
+      <Modal
+        isOpen={!!router.query.transfer}
+        onRequestClose={() => router.push("/")}
+        style={customStyles}
+      >
+        <TransferModal />
+      </Modal>
     </Wrapper>
   );
 };
